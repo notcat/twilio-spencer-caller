0b45ff62378d1c745162152c2c918f1c785c4e08
damon <41898000+notcat@users.noreply.github.com>
2022-02-26 14:30:59 -0800

    completed

--- make-call.js
+++ make-call.js
@@ -1,14 +1,16 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
+require('dotenv').config()
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 client.calls
     .create({
-        url: 'http://demo.twilio.com/docs/voice.xml',
+        twiml: '<Response><Play>http://174.4.243.139/files/untitled.mp3</Play></Response>',
         to: process.env.OUTGOING_PHONE_NUMBER,
-        from: "",
+        from: "+19035688521",
     })
     .then(call => console.log(call.sid));
